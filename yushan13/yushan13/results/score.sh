bb620ee0ab5d7e2e2b299f2838753521c8ef31ed
Yushan13H <166946903+Yushan13H@users.noreply.github.com>
2025-06-07 23:44:19 -0700

    Delete redundant directory

--- yushan13/yushan13/results/score.sh
+++
@@ -1,4 +0,0 @@
-python /storage/yushan13/LLaVA-Med/llava/eval/eval_multimodal_chat_gpt_score.py \
-    --answers-file /storage/yushan13/results/llava_base_model_results.jsonl \
-    --question-file /storage/yushan13/LLaVA-Med/data/eval/llava_med_eval_qa50_qa.jsonl \
-    --scores-file /storage/yushan13/results/scores_base.jsonl
